f64d3ed25f12334651fa42ceca10fd4d209e5961
Ryan Davison-Pearce <ryan.davison-pearce@racelogic.co.uk>
2020-11-20 17:09:05 +0000

    Added CAN ID to save and load EEProm

--- CanSignal.cpp
+++ CanSignal.cpp
@@ -2,6 +2,7 @@
 
 #include "Arduino.h"
 #include "CanSignal.h"
+#include <EEPROM.h>
 
 CanSignal::CanSignal(){
   
@@ -16,9 +17,15 @@ void CanSignal::UpdateFrame(struct can_frame *frame){
 }
 
 void CanSignal::LoadEEProm(uint32_t * address){
-
+  ID = EEPROM.read(*address);
+  *address += 2;
 }
 
 void CanSignal::SaveEEProm(uint32_t * address){
-
+  uint16_t uint16;
+  EEPROM.get(*address, uint16);
+  if (uint16 != ID ){
+    EEPROM.put(*address, ID );
+  }
+  *address += 2;
 }
